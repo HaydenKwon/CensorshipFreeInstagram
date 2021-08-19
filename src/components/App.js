88a293b86e207fa5e575b55772d035fbfb837e24
unknown <youngchekwon@gmail.com>
2021-08-19 02:11:05 -0400

    Need to display feed as well as submit form data onto blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,69 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Identicon from 'identicon.js';
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import Identicon from 'identicon.js'
+import './App.css'
 import Decentragram from '../abis/Decentragram.json'
 import Navbar from './Navbar'
 import Main from './Main'
 
-
 class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('NO browser extension detected. Please install metamask')
+    }
+  }
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+    if (networkData) {
+      const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+      this.setState({ decentragram})
+      const imagesCount = await decentragram.methods.imageCount().call()
+      this.setState({ imagesCount })
+        
+      this.setState({ loading: false})
+    } else {
+        window.alert('Not deployed to detected network')
+        this.setState({loading : false})
+
+    }
+    }
+
+    captureFile = event => {
+
+        event.preventDefault()
+        const file = event.target.files[0]
+        const reader = new window.FileReader()
+        reader.readAsArrayBuffer(file)
+    
+        reader.onloadend = () => {
+          this.setState({ buffer: Buffer(reader.result) })
+          console.log('buffer', this.state.buffer)
+        }
+    }
 
   constructor(props) {
     super(props)
+
     this.state = {
       account: '',
+      decentragram: null,
+      images: [],
+      loading: true
     }
   }
 
@@ -20,16 +71,17 @@ class App extends Component {
     return (
       <div>
         <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
-            />
-          }
-        }
+        {this.state.loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p> Loading... </p>
+          </div>
+        ) : (
+          <Main
+          />
+        )}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,62 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+  //Store Posts
+  mapping(uint => Image) public images;
+
+  uint public imageCount = 0;
+
+  struct Image{
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  event ImageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+  event ImageTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
+  modifier validPost(string memory _imgHash, string memory _description) {
+    require(bytes(_description).length > 0);
+    require(bytes(_imgHash).length > 0);
+    require(msg.sender != address(0x0));
+    _;
+  } 
+  //Creates Images uses imagecount to use the most recent spot of the array
+  function uploadImage(string memory _imgHash, string memory _description) public validPost(_imgHash, _description) {
+    ++imageCount;
+    images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+    emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+  } 
+
+  //Tip
+  function tipImageOwner(uint _id) public payable {
+    //Make sure id is valid(exists)
+    require(_id > 0 && _id <= imageCount);
+    //Fetches author from image
+    Image memory _image = images[_id];
+    address payable _author = _image.author;
+    //Sends ether to author
+    address(_author).transfer(msg.value);
+    //updates total tipped
+    _image.tipAmount = _image.tipAmount + msg.value;
+    //Trigger an event
+    emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _author);
+  }
+
+  
 }
